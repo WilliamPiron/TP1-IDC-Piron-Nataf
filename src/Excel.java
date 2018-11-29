24479577df9a0595a3a0a5b4dcae0dc0054b5334
William <william.piron77@gmail.com>
2018-11-29 10:40:25 +0100

    Changed floats to doubles

--- src/Excel.java
+++ src/Excel.java
@@ -2,22 +2,57 @@ import com.codoid.products.fillo.Connection;
 import com.codoid.products.fillo.Fillo;
 import com.codoid.products.fillo.Recordset;
 
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class Excel {
 
-
+    private ArrayList<TauxFertilite> listeTaux;
 
     public Excel(){
+        NumberFormat format = NumberFormat.getInstance(Locale.FRANCE);
+        Number number;
+        double tmp;
         Fillo fillo = new Fillo();
+        listeTaux = new ArrayList<TauxFertilite>();
         try {
+            System.setProperty("ROW", "4");
             Connection connection = fillo.getConnection("C:\\Users\\William\\Documents\\Cours\\IDC\\TP1-Rendu\\version2000.xls");
-            String query = "";
+            //String query = "Select 1985, 1990, 1995, 1997, 2000 From Data Where 'Country Code'='FRA' and 'Indicator Code'='SP.ADO.TFRT'";
+            String query = "Select * From Data";
             Recordset recordset = connection.executeQuery(query);
 
             while(recordset.next()){
-                
+                number = format.parse(recordset.getField("1985"));
+                tmp = number.doubleValue();
+                listeTaux.add(new TauxFertilite(1985,tmp));
+                /*
+                number = format.parse(recordset.getField("1990"));
+                tmp = number.doubleValue();
+                listeTaux.add(new TauxFertilite(1990,tmp));
+
+                number = format.parse(recordset.getField("1995"));
+                tmp = number.doubleValue();
+                listeTaux.add(new TauxFertilite(1995,tmp));
+
+                number = format.parse(recordset.getField("1997"));
+                tmp = number.doubleValue();
+                listeTaux.add(new TauxFertilite(1997,tmp));
+
+                number = format.parse(recordset.getField("2000"));
+                tmp = number.doubleValue();
+                listeTaux.add(new TauxFertilite(2000,tmp));*/
             }
+
+            recordset.close();
+            connection.close();
         } catch(Exception e ){
             e.printStackTrace();
         }
+    }
+
+    public ArrayList<TauxFertilite> getListeTaux(){
+        return listeTaux;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args){
+        ArrayList<TauxFertilite> listeTauxExcel = new ArrayList<TauxFertilite>();
 
+        Excel excel = new Excel();
+
+        listeTauxExcel = excel.getListeTaux();
+
+        for(TauxFertilite taux : listeTauxExcel){
+            System.out.println("Annee : "+taux.getId()+" | Taux de fertilité : "+taux.getTaux());
+        }
     }
 }
